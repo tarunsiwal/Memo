13ac57457c2df5dad8f61258eb8ffef834fe9c60
tarun <tarunpanchal277@gmail.com>
2025-10-27 14:10:00 +0530

    added nae=me in footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
-  return (
-    <div className='footer'>&copy; 2025 KARYAM.</div>
-  )
+  return <div className="footer">&copy; 2025 MEMO.</div>;
 }
 
-export default Footer
+export default Footer;
